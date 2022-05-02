0b0b514a39d3a76ceacc3a0f28df755c819c2e3f
William Hwang <willhwang90@gmail.com>
2022-05-02 11:20:06 -0400

    commit tree 2f338404e82975bae85cd8eb25ecf7493f686b9a

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+for(let age = 30; age < 40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+//accepts that accepts two arguments: an array of string names, and an event name. Create a for loop with a counter that starts at 0 and increments at the end of each loop. The for loop should stop once it has iterated over the length of the array. 
+const writeCards = (arr, eventName)=> {
+    return arr.map((name)=> `Thank you, ${name}, for the wonderful ${eventName} gift!`)
+}
